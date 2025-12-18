4b116e729635e92a47453bffd3937c6d608ec171
dolartand <dolartand@gmail.com>
2025-12-18 23:04:25 +0300

    feat: add DAO layer

---
+++ src/main/java/com/innowise/paymentservice/entity/Payment.java
@@ -0,0 +1,43 @@
+package com.innowise.paymentservice.entity;
+
+import com.innowise.paymentservice.enums.PaymentStatus;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Document(collection = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Payment {
+
+    @Id
+    private String id;
+
+    @Field("order_id")
+    @Indexed(unique = true)
+    private Long orderId;
+
+    @Field("user_id")
+    @Indexed(unique = true)
+    private Long userId;
+
+    @Field("status")
+    @Indexed
+    private PaymentStatus status;
+
+    @Field("timestamp")
+    @Indexed
+    private LocalDateTime timestamp;
+
+    @Field("payment_amount")
+    private BigDecimal paymentAmount;
+}
---
+++ src/main/java/com/innowise/paymentservice/repository/PaymentRepository.java
@@ -0,0 +1,43 @@
+package com.innowise.paymentservice.repository;
+
+import com.innowise.paymentservice.entity.Payment;
+import com.innowise.paymentservice.enums.PaymentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends MongoRepository<Payment, String> {
+
+    Optional<Payment> findByOrderId(Long orderId);
+
+    boolean existsByOrderId(Long id);
+
+    List<Payment> findByUserId(Long userId);
+
+    Page<Payment> findByUserId(Long userId, Pageable pageable);
+
+    List<Payment> findByStatus(PaymentStatus status);
+
+    Page<Payment> findByStatus(PaymentStatus status, Pageable pageable);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'userId': ?0, 'timestamp': { '$gte': ?1, '$lte': ?2 } } }",
+            "{ '$group': { '_id': null, 'total': { '$sum': '$paymentAmount' } } }"
+    })
+    BigDecimal getTotalAmountByUserIdAndDateRange(Long userId, LocalDateTime from, LocalDateTime to
+    );
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'timestamp': { '$gte': ?0, '$lte': ?1 } } }",
+            "{ '$group': { '_id': null, 'total': { '$sum': '$paymentAmount' } } }"
+    })
+    BigDecimal getTotalAmountForDateRange(LocalDateTime from, LocalDateTime to);
+}
